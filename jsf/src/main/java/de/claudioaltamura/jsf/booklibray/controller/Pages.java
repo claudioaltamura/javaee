63751749d1d7bd88e718fb26fce00aa01c6d74b8
claudio <claudio@claudioaltamura.de>
2016-02-12 15:09:32 +0100

    little corrections

--- jsf/src/main/java/de/claudioaltamura/jsf/booklibray/controller/Pages.java
+++ jsf/src/main/java/de/claudioaltamura/jsf/booklibray/controller/Pages.java
@@ -2,7 +2,7 @@ package de.claudioaltamura.jsf.booklibray.controller;
 
 public class Pages {
 
-	public static final String EDIT_BOOK = "editBook";
-	public static final String LIST_BOOKS = "listBooks";
+	public static final String EDIT_BOOK = "/booklibrary/editBook";
+	public static final String LIST_BOOKS = "/booklibrary/listBooks";
 
 }
