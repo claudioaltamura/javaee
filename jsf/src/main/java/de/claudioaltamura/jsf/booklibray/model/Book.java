3d17109db242aa4f08f5a501726027284253fa14
claudio <claudio@claudioaltamura.de>
2016-04-06 12:59:07 +0200

    added simple ajax exampl

--- jsf/src/main/java/de/claudioaltamura/jsf/booklibray/model/Book.java
+++ jsf/src/main/java/de/claudioaltamura/jsf/booklibray/model/Book.java
@@ -3,10 +3,15 @@ package de.claudioaltamura.jsf.booklibray.model;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Book implements Serializable  {
 
 	private static final long serialVersionUID = 1L;
+	
+	private final static Logger LOGGER = Logger.getLogger(Book.class.getName()); 
 
 	private Date date;
 	private String title;
@@ -15,69 +20,80 @@ public class Book implements Serializable  {
 	private Author author = new Author();
 	private Publisher publisher = new Publisher();
 	private BigDecimal price = new BigDecimal(0.0);
+	private String description = "";
+	private AtomicLong counter = new AtomicLong(0);
 	
 	public Date getDate() {
 		return date;
 	}
-	
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	
 	public String getTitle() {
 		return title;
 	}
-	
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
 	public int getPages() {
 		return pages;
 	}
-
 	public void setPages(int pages) {
 		this.pages = pages;
 	}
-	
 	public String getIsbn() {
 		return isbn;
 	}
-	
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
-	
 	public Author getAuthor() {
 		return author;
 	}
-	
 	public void setAuthor(Author author) {
 		this.author = author;
 	}
-	
 	public Publisher getPublisher() {
 		return publisher;
 	}
-	
 	public void setPublisher(Publisher publisher) {
 		this.publisher = publisher;
 	}
-	
 	public BigDecimal getPrice() {
 		return price;
 	}
-
 	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
-
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	public void generateDescription() {
+		LOGGER.log(Level.INFO, "generate description== 0");
+		long cnt = counter.incrementAndGet();
+		String generatedDescription = "good book"; 
+		if(cnt % 2 == 0) {
+			generatedDescription = "very good book";
+		}
+	
+		this.description += " - " + generatedDescription;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((author == null) ? 0 : author.hashCode());
 		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
 		result = prime * result + ((isbn == null) ? 0 : isbn.hashCode());
 		result = prime * result + pages;
 		result = prime * result + ((price == null) ? 0 : price.hashCode());
@@ -86,7 +102,7 @@ public class Book implements Serializable  {
 		result = prime * result + ((title == null) ? 0 : title.hashCode());
 		return result;
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -105,6 +121,11 @@ public class Book implements Serializable  {
 			if (other.date != null)
 				return false;
 		} else if (!date.equals(other.date))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
 			return false;
 		if (isbn == null) {
 			if (other.isbn != null)
@@ -135,7 +156,8 @@ public class Book implements Serializable  {
 	public String toString() {
 		return "Book [date=" + date + ", title=" + title + ", pages=" + pages
 				+ ", isbn=" + isbn + ", author=" + author + ", publisher="
-				+ publisher + ", price=" + price + "]";
+				+ publisher + ", price=" + price + ", description="
+				+ description + "]";
 	}
 
 }
