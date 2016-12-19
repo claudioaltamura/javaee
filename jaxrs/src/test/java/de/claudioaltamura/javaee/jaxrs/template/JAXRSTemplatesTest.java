4aa24b1f5729635be07579609f174baa0b492eed
claudio <claudio@claudioaltamura.de>
2016-12-19 13:28:48 +0100

    JAXRS Templates

---
+++ jaxrs/src/test/java/de/claudioaltamura/javaee/jaxrs/template/JAXRSTemplatesTest.java
@@ -0,0 +1,32 @@
+package de.claudioaltamura.javaee.jaxrs.template;
+
+import static org.junit.Assert.*;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JAXRSTemplatesTest {
+
+    private Client client;
+    private WebTarget webTarget;
+
+    @Before
+    public void initClient() {
+        this.client = ClientBuilder.newClient();
+        this.webTarget = this.client.target("https://en.wikipedia.org/wiki/{keyword}");
+    }
+
+    @Test
+    public void contentContainsKeyword() {
+        String keyword = "Java";
+        String content = this.webTarget.resolveTemplate("keyword", keyword).
+                request().
+                get(String.class);
+        assertTrue(content.contains(keyword));
+    }
+
+}
