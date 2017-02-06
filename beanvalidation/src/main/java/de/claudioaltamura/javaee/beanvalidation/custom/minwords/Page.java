92ed10dc26e9779de40618d9e8333995be0f5dd5
claudio <claudio@claudioaltamura.de>
2017-02-06 12:43:31 +0100

    added page class

---
+++ beanvalidation/src/main/java/de/claudioaltamura/javaee/beanvalidation/custom/minwords/Page.java
@@ -0,0 +1,15 @@
+package de.claudioaltamura.javaee.beanvalidation.custom.minwords;
+
+public class Page {
+
+	private String pageContent;
+
+	public String getPageContent() {
+		return pageContent;
+	}
+
+	public void setPageContent(String pageContent) {
+		this.pageContent = pageContent;
+	}
+
+}
